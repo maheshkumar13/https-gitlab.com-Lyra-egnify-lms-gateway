2e06c397e972689d246dd86c7ce0f1084f045189
aslam <asalm@egnify.com>
2018-03-20 17:19:07 +0530

    filters added in the students part

--- src/graphql/settings/student/student.query.js
+++ src/graphql/settings/student/student.query.js
@@ -99,7 +99,12 @@ export const Students = {
       body: JSON.stringify(args),
 	    headers: { 'Content-Type': 'application/json' },//eslint-disable-line
     })
-      .then(response => response.json())
+      .then((response) => {
+        if (response.status >= 400) {
+          return new Error(response.statusText);
+        }
+        return response.json();
+      })
       .then((json) => {
         const data = {};
         data.page = json.students;
