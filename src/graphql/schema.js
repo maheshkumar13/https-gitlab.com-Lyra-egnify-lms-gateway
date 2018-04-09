40bca65d774fb1f861a0119b2232e8d50bb4b82d
aslam <asalm@egnify.com>
2018-04-09 11:35:00 +0530

    Graph QL added for update test with any given field

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -11,8 +11,8 @@ import {
 
 import GradeSystem from './settings/grade/grade.query';
 import TestPattern from './settings/testPattern/testPattern.query';
-import { Tests, QuestionTypes } from './test-management/test/test.query';
-import { removeTest, createDummyTest, createDuplicateTest, createTest } from './test-management/test/test.mutation';
+import { Tests, QuestionTypes, DefaultMarkingSchemas, TestHierarchyNodes } from './test-management/test/test.query';
+import { removeTest, createDummyTest, createDuplicateTest, createTest, updateTest } from './test-management/test/test.mutation';
 import { Curriculum } from './settings/curriculum/curriculum.query';
 import { SubjectList, SubjectTaxonomy } from './settings/subject/subjectTaxonomy.query';
 import { removeSubjectTaxonomy, updateSubjectTaxonomy, createSubjects } from './settings/subject/subjectTaxonomy.mutation';
@@ -54,6 +54,8 @@ new ObjectType({
       LevelFilters,
       StudentUniqueValues,
       StudentsByLastNode,
+      DefaultMarkingSchemas,
+      TestHierarchyNodes,
     },
   }),
   mutation: new ObjectType({
@@ -85,6 +87,7 @@ new ObjectType({
       createDuplicateTest,
       createInstituteHierarchyNodesFromCSV,
       createTest,
+      updateTest,
     },
   }),
 });
