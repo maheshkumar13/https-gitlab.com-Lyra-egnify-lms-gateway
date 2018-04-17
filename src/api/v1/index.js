d01d1e02d3b7e56373c5ff98b5816c896a600cbf
Rahul <rahul.islam3@gmail.com>
2018-04-17 11:48:24 +0530

    add question parser proxy

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,14 +2,24 @@
  * API
  */
 
+import { config } from '../../config/environment';
+
+const request = require('request');
 const uploadFile = require('./uploadFile');
+const user = require('./user');
+const auth = require('./../../auth').default;
+
 // const conceptTaxonomy = require('./conceptTaxonomy');
 
 export default function (app) {
   //  Insert API below
   app.use('/api/v1/uploadFile', uploadFile);
-  app.use('/api/v1/users', require('./user'));
+  app.use('/api/v1/users', user);
+  app.use('/auth', auth);
 
-  app.use('/auth', require('./../../auth').default);
-  
+  // Proxy Request below
+  app.post('/api/v1/question/populateQuestion', (req, res) => request({
+    url: `${config.services.test}/api/v1/question/populateQuestion`,
+    method: 'POST',
+  }).pipe(res));
 }
