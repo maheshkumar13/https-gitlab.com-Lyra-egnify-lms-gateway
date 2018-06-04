1d771940abc2f31ea3a921b961d01a71289814d9
aslam <asalm@egnify.com>
2018-06-04 14:53:43 +0530

    changed api order in index

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -11,13 +11,17 @@ const user = require('./user');
 const auth = require('./../../auth').default;
 // const conceptTaxonomy = require('./conceptTaxonomy');
 
+
 export default function (app) {
   //  Insert API below
+
+
   app.use('/api/v1/uploadFile', uploadFile);
   app.use('/api/v1/users', user);
   app.use('/auth', auth);
 
   // Proxy Request below
+
   app.post('/api/v1/question/populateQuestion', authService.isAuthenticated(), (req, res) => {
     const submissionUrl = `${config.services.test}/api/v1/question/populateQuestion`;
     const submissionRequest = request(submissionUrl);
