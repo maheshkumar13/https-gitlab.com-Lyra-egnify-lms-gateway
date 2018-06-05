11cb570efea1afc6117a14ebe0eb3f0e0d03901f
sattyboy <ksathyasairam@gmail.com>
2018-06-05 17:00:09 +0530

    Multi Tenancy - Question Mapping Upload BF-2

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,7 +1,8 @@
 /**
  * API
  */
-
+import stream from 'stream';
+import _ from 'lodash';
 import { config } from '../../config/environment';
 import * as authService from '../../auth/auth.service';
 
@@ -10,6 +11,9 @@ const request = require('request');
 const uploadFile = require('./uploadFile');
 const user = require('./user');
 const auth = require('./../../auth').default;
+
+const { Duplex } = stream;
+
 // const conceptTaxonomy = require('./conceptTaxonomy');
 const upload = multer({
   storage: multer.MemoryStorage,
@@ -18,6 +22,12 @@ const upload = multer({
   },
 });
 
+function bufferToStream(buffer) {
+  const duplexStream = new Duplex();
+  duplexStream.push(buffer);
+  duplexStream.push(null);
+  return duplexStream;
+}
 export default function (app) {
   //  Insert API below
   app.use('/api/v1/uploadFile', uploadFile);
@@ -31,33 +41,51 @@ export default function (app) {
     upload.single('file'),
     (req, res) => {
       const submissionUrl = `${config.services.test}/api/v1/question/populateQuestion`;
-      const userContext = req.user._doc;
-      const { file } = req;
+      const { body } = req;
+      const formData = {};
+      _.forEach(body, (value, key) => {
+        formData[key] = JSON.stringify(value);
+      });
+      formData.user = JSON.stringify(req.user);
+      if (req.file) {
+        formData.file = {
+          value: bufferToStream(req.file.buffer),
+          options: {
+            filename: req.file.originalname,
+            contentType: req.file.mimetype,
+            knownLength: req.file.size,
+          },
+        };
+      }
 
-      const { question_reg } = req.body;
-      const { option_reg } = req.body;
-      const { subjects } = req.body;
-      const { no_of_questions } = req.body;
-      const { testId } = req.body;
-
-      request.post(submissionUrl, {
-        form: {
-          user: userContext,
-          file,
-          question_reg,
-          option_reg,
-          subjects,
-          no_of_questions,
-          testId,
-        },
-      }).pipe(res);
+      request.post(submissionUrl, { formData }).pipe(res);
     },
   );
 
 
-  app.post('/api/student/createBulkStudents', authService.isAuthenticated(), (req, res) => {
-    const submissionUrl = `${config.services.settings}/api/student/createBulkStudents`;
-    const submissionRequest = request(submissionUrl);
-    req.pipe(submissionRequest).pipe(res);
-  });
+  app.post(
+    '/api/v1/student/createBulkStudents',
+    authService.isAuthenticated(),
+    upload.single('file'),
+    (req, res) => {
+      const submissionUrl = `${config.services.settings}/api/student/createBulkStudents`;
+      const { body } = req;
+      const formData = {};
+      _.forEach(body, (value, key) => {
+        formData[key] = JSON.stringify(value);
+      });
+      formData.user = JSON.stringify(req.user);
+      if (req.file) {
+        formData.file = {
+          value: bufferToStream(req.file.buffer),
+          options: {
+            filename: req.file.originalname,
+            contentType: req.file.mimetype,
+            knownLength: req.file.size,
+          },
+        };
+      }
+      request.post(submissionUrl, { formData }).pipe(res);
+    },
+  );
 }
