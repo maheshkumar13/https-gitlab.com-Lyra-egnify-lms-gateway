5eceadbc70ab6c48a747a46739479e5c0678af75
sattyboy <ksathyasairam@gmail.com>
2018-06-05 23:03:33 +0530

    Request Masking

--- src/auth/local/index.js
+++ src/auth/local/index.js
@@ -8,13 +8,15 @@ router.post('/', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     const error = err || info;
     if (error) {
+      console.info('here');
+
       return res.status(401).json(error);
     }
     if (!user) {
       return res.status(404).json({ message: 'Something went wrong, please try again.' });
     }
 
-    const token = signToken(user._id, user.role, user.instituteId);
+    const token = signToken(user._id, user.role, user.instituteId, user.hostname);
     res.json({ token });
   })(req, res, next);
 });
