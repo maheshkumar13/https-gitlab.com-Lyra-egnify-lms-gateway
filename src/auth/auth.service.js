d3d5d8bf3453a877b66395b8b35d04c0dbe7eebc
sattyboy <ksathyasairam@gmail.com>
2018-06-29 12:20:23 +0530

    Sorting Common Analysis

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -70,7 +70,7 @@ export function signToken(id, role, instituteId, hostname) {
   return jwt.sign({
     _id: id, role, instituteId, hostname,
   }, config.secrets.session, {
-    expiresIn: 60 * 60 * 5,
+    expiresIn: 60 * 60,
   });
 }
 
