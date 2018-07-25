c0c19cc4a54c5cc1a5ce99c3490db2ecbd8144cc
Unknown <chauhangaurav101@gmail.com>
2018-07-25 11:42:50 +0530

    edit added

--- src/graphql/settings/student/student.mutation.js
+++ src/graphql/settings/student/student.mutation.js
@@ -10,11 +10,12 @@ import {
   // GraphQLInt as IntType,
   GraphQLString as StringType,
   GraphQLEnumType,
+
 } from 'graphql';
 import GraphQLJSON from 'graphql-type-json';
 import fetch from '../../../utils/fetch';
 import { config } from '../../../config/environment';
-import StudentType from './student.type';
+import {  StudentType } from './student.type';
 
 const GraphQLDate = require('graphql-date');
 
@@ -140,9 +141,43 @@ export const deleteStudent = {
   },
 };
 
+export const editStudent = {
+  args: {
+    studentId: { type: new NonNull(StringType) },
+    studentData: { type: StudentInputType },
+  },
+  type: new List(StudentType),
+  async resolve(obj, args, context) {
+    args.hierarchy = JSON.stringify(args.hierarchy);//eslint-disable-line
+    const url = `${config.services.settings}/api/student/edit/studentList`;
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(args),
+	    headers: { 'Content-Type': 'application/json' },//eslint-disable-line
+    }, context)
+      .then((response) => {
+        if (response.status >= 400) {
+          return new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(json =>
+        json)
+      .catch((err) => {
+        console.error(err);
+        return err.json();
+      })
+      .catch((errjson) => { //eslint-disable-line
+        // console.log(errjson);
+      });
+  },
+  description: 'This will take csv file url as input and populate the students',
+};
+
 
 export default{
   createStudent,
   createManyStudents,
   deleteStudent,
+  editStudent,
 };
--- src/graphql/settings/student/student.type.js
+++ src/graphql/settings/student/student.type.js
@@ -9,12 +9,13 @@ import {
   GraphQLString as StringType,
   // GraphQLNonNull as NonNull,
   // GraphQLInt as IntType,
+  GraphQLInputObjectType as InputObjectType,
 } from 'graphql';
 import GraphQLJSON from 'graphql-type-json';
 
 // const GraphQLDate = require('graphql-date');
 
-const StudentType = new ObjectType({
+export const StudentType = new ObjectType({
   name: 'StudentType',
   fields: {
     egnifyId: { type: StringType },
@@ -30,4 +31,23 @@ const StudentType = new ObjectType({
   },
 });
 
-export default StudentType;
+export const StudentInputType = new InputObjectType({
+  name: 'StudentInputType',
+  fields: {
+    egnifyId: { type: StringType },
+    studentId: { type: StringType },
+    studentName: { type: StringType },
+    fatherName: { type: StringType },
+    phone: { type: StringType },
+    email: { type: StringType },
+    gender: { type: StringType },
+    dob: { type: StringType },
+    category: { type: StringType },
+    hierarchy: { type: GraphQLJSON },
+  },
+});
+
+export default{
+  StudentInputType,
+  StudentType,
+};
