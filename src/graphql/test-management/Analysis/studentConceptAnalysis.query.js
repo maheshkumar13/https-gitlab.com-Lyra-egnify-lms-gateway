a54e3ba5a0ce4ee655fe8707e82acc441364f231
Biswa <biswa@egnify.com>
2018-09-04 18:51:40 +0530

    allStudentsconceptanalysysis API added

--- src/graphql/test-management/Analysis/studentConceptAnalysis.query.js
+++ src/graphql/test-management/Analysis/studentConceptAnalysis.query.js
@@ -3,7 +3,7 @@ import {
   GraphQLString as StringType,
   GraphQLNonNull as NonNull,
   // GraphQLBoolean as BooleanType,
-  // GraphQLInt as IntType,
+  GraphQLInt as IntType,
   // GraphQLList as List,
   // GraphQLFloat as FloatType,
   // GraphQLInputObjectType as InputObjectType,
@@ -38,6 +38,32 @@ export const StudentConceptAnalysis = {
   },
 };
 
+export const allStudentConceptAnalysis = {
+  args: {
+    limit: { type: IntType, description: 'number of imtems per page' },
+    pageNumber: { type: IntType, description: 'unique identifier for page number' },
+    testId: { type: StringType, description: 'Unique identifier for the test' },
+  },
+  type: GraphQLJSON,
+  async resolve(obj, args, context) {
+    const url = `${config.services.test}/api/v1/Analysis/allStudentConceptAnalysis`;
+
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(args),
+	    headers: { 'Content-Type': 'application/json' },//eslint-disable-line
+    }, context)
+      .then((response) => {
+        if (response.status >= 400) {
+          return new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(json => json);
+  },
+};
+
 export default{
   StudentConceptAnalysis,
+  allStudentConceptAnalysis,
 };
