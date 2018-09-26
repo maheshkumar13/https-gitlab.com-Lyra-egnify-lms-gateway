0e77f3257675c1f6ccf794c53944b82d6dc879ef
aslam <asalm@egnify.com>
2018-09-26 09:35:36 +0530

    rank analysis added

--- src/graphql/test-management/comparisonAnalysis/comparisonRankAnalysis.type.js
+++ src/graphql/test-management/comparisonAnalysis/comparisonRankAnalysis.type.js
@@ -2,7 +2,7 @@ import {
   // GraphQLObjectType as ObjectType,
   GraphQLString as StringType,
   GraphQLNonNull as NonNull,
-  // GraphQLBoolean as BooleanType,
+  GraphQLBoolean as BooleanType,
   GraphQLInt as IntType,
   GraphQLList as List,
   // GraphQLFloat as FloatType,
@@ -17,7 +17,17 @@ export const ComparisonRankAnalysisHierarchyInputType = new InputObjectType({
   fields: {
     child: { type: new NonNull(StringType), description: 'Name of the hierarchy node' },
     childCode: { type: new NonNull(StringType), description: 'Internal code of the hierarchy node' },
-    level: { type: new NonNull(StringType), description: 'Level number of the hierarchy node' },
+    level: { type: new NonNull(IntType), description: 'Level number of the hierarchy node' },
+  },
+});
+
+export const ComparisonRankAnalysisParentInputType = new InputObjectType({
+  name: 'ComparisonRankAnalysisParentInputType',
+  description: 'Input type for parent node',
+  fields: {
+    child: { type: new NonNull(StringType), description: 'Name of the hierarchy node' },
+    childCode: { type: new NonNull(StringType), description: 'Internal code of the hierarchy node' },
+    level: { type: new NonNull(IntType), description: 'Level number of the hierarchy node' },
   },
 });
 
@@ -27,8 +37,10 @@ export const ComparisonRankAnalysisInputType = new InputObjectType({
   fields: {
     testIds: { type: new List(new NonNull(StringType)), description: 'testIds' },
     hierarchies: { type: new List(ComparisonRankAnalysisHierarchyInputType), description: 'Input type for hierarchies in comaprison analysis' },
-    viewLevel: { type: new NonNull(int), description: 'Level number of the hierarchy nodes to display data' },
+    viewLevel: { type: new NonNull(IntType), description: 'Level number of the hierarchy nodes to display data' },
     topLimit: { type: new NonNull(IntType), description: 'Number of top students' },
+    parent: { type: ComparisonRankAnalysisParentInputType, description: 'Input type for parent node' },
+    studentData: { type: BooleanType, description: 'true to get student data' },
   },
 });
 
