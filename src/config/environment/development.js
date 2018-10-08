47468986dcaaae76d6ad7d4908c732e65660a72b
sattyboy <ksathyasairam@gmail.com>
2018-10-08 17:15:47 +0530

    Dev Setup Changes

--- src/config/environment/development.js
+++ src/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/tenantregistry-sso-qa',
+    uri: 'mongodb://localhost/tenantregistry-sso',
   },
   // Seed database on startup
   seedDB: true,
