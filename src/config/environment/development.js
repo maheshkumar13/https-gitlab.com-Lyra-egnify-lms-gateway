0226c5abc591ce621d6323fc45f1dc3207c9564d
sattyboy <sathya@egnify.com>
2018-11-29 15:06:13 +0530

    Conflit Resolve

--- src/config/environment/development.js
+++ src/config/environment/development.js
@@ -10,7 +10,7 @@ module.exports = {
   // Seed database on startup
   seedDB: true,
   services: {
-    settings: 'http://localhost:5003',
+    settings: 'http://localhost:5001',
     test: 'http://localhost:5002',
     sso: 'http://localhost:3002',
   },
