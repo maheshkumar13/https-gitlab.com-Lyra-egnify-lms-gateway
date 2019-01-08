98d0a5e6734746eb5c1e27c20c0c70bb585679f6
bvemula <bharath@egnigfy.com>
2019-01-08 10:49:22 +0530

    time spent variable added

--- src/graphql/test-management/Analysis/studentBehaviourData.type.js
+++ src/graphql/test-management/Analysis/studentBehaviourData.type.js
@@ -19,6 +19,7 @@ export const BehaviourDataType = new ObjectType({
     questionNumber: { type: IntType, description: 'question Number' },
     subject: { type: StringType, description: 'name of the subject' },
     status: { type: StringType, description: 'question status C/W/U/P' },
+    timeSpent: { type: IntType, description: 'total time spent for thq question in milliseconds' },
     enterStatus: { type: BooleanType, description: 'collean variable to know if student has viewed the question atlkeast for once' },
     qMap: { type: QuestionMappingsDataType, description: 'question mapping type' },
     marks: { type: IntType, description: 'marks attained for the question' },
