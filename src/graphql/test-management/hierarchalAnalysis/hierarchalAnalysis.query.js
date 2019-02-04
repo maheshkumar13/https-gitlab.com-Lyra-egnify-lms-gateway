e65bc28e09b94da6d73abc527b9664887aeb6c08
Aakash Parsi <aakash@egnify.com>
2019-02-04 12:43:15 +0530

    admin login has been handled

--- src/graphql/test-management/hierarchalAnalysis/hierarchalAnalysis.query.js
+++ src/graphql/test-management/hierarchalAnalysis/hierarchalAnalysis.query.js
@@ -22,7 +22,7 @@ export const HierarchalAnalysisForStudentProfile = {
   args: {
     input: {
       type: new NonNull(HierarchicalAnalysisInputType),
-      description: 'Inputs for comparison error analysis',
+      description: 'Inputs for hierarchical analysis',
     },
   },
   type: HierarchicalAnalysisOutputType,
@@ -43,6 +43,32 @@ export const HierarchalAnalysisForStudentProfile = {
   },
 };
 
+export const HierarchalAnalysis = {
+  args: {
+    input: {
+      type: new NonNull(HierarchicalAnalysisInputType),
+      description: 'Inputs for hierarchical analysis',
+    },
+  },
+  type: HierarchicalAnalysisOutputType,
+  async resolve(obj, args, context) {
+    const url = `${config.services.test}/api/v1/hierarchalAnalysis/getTestStats`;
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(args.input),
+	    headers: { 'Content-Type': 'application/json' },//eslint-disable-line
+    }, context)
+      .then((response) => {
+        if (response.status >= 400) {
+          return new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(json => json);
+  },
+};
+
 export default {
   HierarchalAnalysisForStudentProfile,
+  HierarchalAnalysis,
 };
