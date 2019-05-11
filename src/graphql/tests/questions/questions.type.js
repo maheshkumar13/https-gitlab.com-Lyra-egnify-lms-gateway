8d99f33415176e509881b23cb4c495fd4eb105d0
aslam2 <aslam@egnify.com>
2019-05-11 13:03:32 +0530

    hint,solution,skill added in question model

--- src/graphql/tests/questions/questions.type.js
+++ src/graphql/tests/questions/questions.type.js
@@ -37,8 +37,12 @@ export const QuestionType = new ObjectType({
     difficulty: { type: StringType, description: 'Difficulty' },
     revised_blooms_taxonomy: { type: StringType, description: 'revised_blooms_taxonomy' },
     questionTypeMetaData: { type: GraphQLJSON, description: 'Metadata' },
+    hint: { type: StringType, description: 'Hint'},
+    skill: { type: StringType, description: 'Skill' },
+    solution: { type: StringType, description: 'Solution' }
   },
 });
+
 
 export const ResultInputType = new InputType({
   name: 'ResultInputType',
