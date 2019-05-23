c7a4b56abfc86450a7a14a47e38ace58c3763779
aslam2 <aslam@egnify.com>
2019-05-23 14:10:48 +0530

    isMandatory added in getsubjectTextbooktopic

--- src/api/settings/subject/subject.controller.js
+++ src/api/settings/subject/subject.controller.js
@@ -165,7 +165,7 @@ export async function getSubjectTextbookTopic(args, context) {
         ]
       }
     }
-    return Subject.find(subjectQuery,{_id: 0, subject: 1, code: 1}).cache(config.cacheTimeOut.subject).then((subjects) => {
+    return Subject.find(subjectQuery,{_id: 0, subject: 1, code: 1, isMandatory: 1}).cache(config.cacheTimeOut.subject).then((subjects) => {
       const subjectcodes = subjects.map( x => x.code)
       const textbookQuery = {
         active: true,
