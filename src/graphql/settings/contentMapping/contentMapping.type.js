2e96926b016de6c7a3e4fea72afc8e60544fbe17
adi1eg <aditi.egnify@gmail.com>
2019-06-24 11:53:50 +0530

    added id to categorywisefiles API, filtered data based on mongoid in getFileData API

--- src/graphql/settings/contentMapping/contentMapping.type.js
+++ src/graphql/settings/contentMapping/contentMapping.type.js
@@ -112,6 +112,7 @@ const pageInfoType = new ObjectType({
 const categoryListType = new ObjectType({
   name: 'categoryListType',
   fields: {
+    id :{type: StringType ,description :'mongodb id'},
     content: { type: contentType, description: 'content json' },
     resource: { type: resourceType, description: 'resource of the file' },
     textbookCode: { type: StringType, description: 'code of the textbook' },
@@ -129,8 +130,9 @@ export const CategoryWiseFilesOutputType = new ObjectType({
 export const FileDataInputType = new InputType({
   name: 'FileDataInputType',
   fields: {
-    fileKey: { type: new NonNull(StringType), description: 'Key of the file' },
-    textbookCode: { type: new NonNull(StringType), description: 'code of textbook ' },
+    // fileKey: { type: new NonNull(StringType), description: 'Key of the file' },
+    // textbookCode: { type: new NonNull(StringType), description: 'code of textbook ' },
+    id :{ type: new NonNull(StringType), description : 'mongodb id'}
   },
 });
 
