88bcba4df9eef01ffc0d726f6f2a247bd16b4b6a
Shreyas1517 <shreyas.egnify@gmail.com>
2019-07-02 16:09:18 +0530

    WIP

---
+++ src/api/settings/package/package.controller.js
@@ -0,0 +1,105 @@
+import { getModel as packageModel } from './package.model';
+import { getModel as textbookModel } from '../textbook/textbook.model';	
+
+/**
+@description This function displays the package details for a given packageId
+
+@author Shreyas
+@date   23/03/2018
+*/
+
+export async function getPackageDetails(args, context) {
+  const query1 = {
+    active: true, 
+    packageId: args.input,
+  };
+  const projection1 = {
+    packageName: 1,
+    academicYear: 1,
+    reviewedBy: 1,
+    authoredBy: 1,
+    classCode: 1,
+    orientations: 1,
+    branches: 1,  
+    studentIds: 1,
+    subjects: 1,
+    feedback: 1,
+  };
+  return packageModel(context).then(Package => {
+    return (Package.findOne(query1, projection1)).then((packageObj) => {
+      if(!packageObj) {
+        throw new Error("Package not found");
+      }
+      const finalResult = {};
+  
+      finalResult.packageName = packageObj.packageName;
+      finalResult.academicYear = packageObj.academicYear;
+      finalResult.reviewedBy = packageObj.reviewedBy;
+      finalResult.authoredBy = packageObj.authoredBy;  
+      finalResult.class = {};
+      finalResult.class.code = packageObj.classCode;                        
+      finalResult.orientations = packageObj.orientations;
+      finalResult.branches = packageObj.branches;
+      finalResult.students = packageObj.studentIds;
+      if(context.email === packageObj.authoredBy) {
+        finalResult.feedback = packageObj.feedback;
+      }
+      else {
+        finalResult.feedback = null;
+      }
+      finalResult.subjects = [];
+      const subjArray = packageObj.subjects;
+      const textbookCodesArray = [];
+      for (let i = 0; i<subjArray.length; i++) {
+        for (let j = 0; j<subjArray[i].textbookCodes.length; j++) {
+          textbookCodesArray.push(subjArray[i].textbookCodes[j]);
+        }
+      }
+      const query2 = {
+        code: { $in : textbookCodesArray},
+      };
+      const projection2 = {
+        "name": 1,
+        "code": 1,
+        "refs.subject.name": 1,
+        "refs.subject.code": 1,
+        "refs.class.name": 1,
+        "refs.class.code": 1,
+      };
+      const group2 = {
+        _id: {
+          subject: {
+            name: "$refs.subject.name",
+            code: "$refs.subject.code",
+          },
+          class: {
+            name: "$refs.class.name",
+          },
+        },
+        textBooks: {
+          $addToSet: {
+            "textbookCode": "$code",
+            "textbookName": "$name",
+          }
+        }
+      }
+       return textbookModel(context).then(Textbook => {
+        return (Textbook.aggregate([
+          { $match: query2 },
+          { $project: projection2 },
+          { $group: group2 }        
+        ]).allowDiskUse(true)).then(result1 => {
+          finalResult.class.name = result1[0]._id.class.name;
+          for (let i=0; i<result1.length; i++) {
+            const obj = {};
+            obj.subjectName = result1[i]._id.subject.name;
+            obj.subjectCode = result1[i]._id.subject.code;
+            obj.textBooks = result1[i].textBooks;
+            finalResult.subjects.push(obj);
+          }
+          return finalResult;
+        });
+      });
+    }).catch(err => err);
+  });
+}
---
+++ src/api/settings/package/package.model.js
@@ -0,0 +1,36 @@
+import { getDB } from '../../../db';
+
+const mongoose = require('mongoose');
+
+const subjectSchema = new mongoose.Schema({
+  subjectCode: { type: String },
+  textbookCodes: { type: [String] },
+});
+
+const PackageSchema = new mongoose.Schema({
+  packageName: { type: String },
+  packageId: { type: String },
+  academicYear: { type: String },
+  classCode: { type: String },
+  subjects: { type: [subjectSchema] },
+  orientations: { type: [String] },
+  branches: { type: [String] },
+  studentIds: { type: [String], default: null },
+  active: { type: Boolean, default: true },
+  status: { type: String, enum: ['underReview', 'underFeedback', 'approved'], default: 'underReview' },
+  reviewedBy: { type: String },
+  authoredBy: { type: String },
+  feedback: { type: String },
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+});
+
+export async function getModel(userCxt) {
+  const { instituteId } = userCxt;
+  const db = await getDB(instituteId);
+  return db.model('Package', PackageSchema);
+}
+
+export default {
+  getModel,
+};
