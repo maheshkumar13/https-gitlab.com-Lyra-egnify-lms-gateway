390ac62c8b6c4ae0db5de6ffbea412eb153d0e3b
aslam2 <aslam@egnify.com>
2019-09-03 17:17:32 +0530

    rankguru-uat folder added

--- src/config/environment/qa.js
+++ src/config/environment/qa.js
@@ -32,4 +32,5 @@ module.exports = {
     host: process.env.REDIS_HOST || '35.244.34.84',
     password: process.env.REDIS_PASSWORD || 'rQsVPF2gbiHi',
   },
+  AWS_PRIVATE_BUCKET: 'rankguru-uat',
 };
