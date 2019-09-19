4cdf9beb80de96306933c917845fbaab0f7fe39c
Mohit <mohit@egnify.com>
2019-09-19 12:42:41 +0530

    starttest removed from GRAPHQL

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -33,7 +33,7 @@ import {PackageList, PackageDetails} from './settings/package/package.query';
 import {UpdatePackage,FeedbackPackage} from './settings/package/package.mutation';
 import { autoComplete , searchResult } from './search/searchRequest/search.query'
 import { addTimeseries } from './analysis/timeseries/timeseries.query';
-import { ListTest , UpcomingTests , ListSubjectWiseBooksAndTestCount, CompletedTests , StartTest , HeaderCountForTextBookBasedTest} from '../graphql/tests/upload/upload.query';
+import { ListTest , UpcomingTests , ListSubjectWiseBooksAndTestCount, CompletedTests ,  HeaderCountForTextBookBasedTest} from '../graphql/tests/upload/upload.query';
 import { ListMarkingSchema } from '../graphql/tests/markingShema/marking.shema.query';
 import { PublishTest , ParseAndValidateTest , updateTestInfo} from './tests/upload/upload.mutation'
 
@@ -80,7 +80,6 @@ const schema = new Schema({
       UpcomingTests,
       ListSubjectWiseBooksAndTestCount,
       CompletedTests,
-      StartTest,
       HeaderCountForTextBookBasedTest
     },
   }),
