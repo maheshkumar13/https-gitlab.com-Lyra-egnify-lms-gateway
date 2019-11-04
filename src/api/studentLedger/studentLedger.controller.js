00dcd6cdc4ac1e1c557ec7b033c7cfe7e90a243f
NikhilKrDwivedi <authornikhildwivedi@gmail.com>
2019-11-04 18:16:12 +0530

    Coin Complete

--- src/api/studentLedger/studentLedger.controller.js
+++ src/api/studentLedger/studentLedger.controller.js
@@ -2,10 +2,10 @@
  @author Nikhil Kumar
  @date    23/01/2018
 */
-import { getModel as studentLedgerModel } from './studentLedger.model';
-import { getModel as contentMappingModel } from '../settings/contentMapping/contentMapping.model';
-import { getModel as textbookModel } from '../settings/textbook/textbook.model';
-import { getModel as studentModel } from '../settings/student/student.model';
+import { getModel as StudentLedgerModel } from './studentLedger.model';
+import { getModel as ContentMappingModel } from '../settings/contentMapping/contentMapping.model';
+import { getModel as TextbookModel } from '../settings/textbook/textbook.model';
+import { getModel as StudentModel } from '../settings/student/student.model';
 
 
 export async function creditCoin(req, res) {
@@ -16,59 +16,57 @@ export async function creditCoin(req, res) {
       return res.status(400).send("Asset Id is missing.");
     }
 
-    const [studentLedger, contentMapping, student, textbook] = await Promise.all(
-      [studentLedgerModel(req.user_cxt),
-      contentMappingModel(req.user_cxt),
-      studentModel(req.user_cxt),
-      textbookModel(req.user_cxt)
+    const [
+      StudentLedger,
+      ContentMapping,
+      Student,
+      Textbook] = await Promise.all([
+        StudentLedgerModel(req.user_cxt),
+        ContentMappingModel(req.user_cxt),
+        StudentModel(req.user_cxt),
+        TextbookModel(req.user_cxt)
       ]);
 
     //Check for if AssetId Is present in collection or not!!!
-    const confirmation = await contentMapping.findOne({ "assetId": assetId });
-    if (!confirmation) {
+    const assetData = await ContentMapping.findOne({ assetId, active: true });
+    if (!assetData) {
       return res.status(404).send("this is invalid asset id...");
     }
 
     //validate assetId If this id is belong to studentId
-    const getStudentOrientation = student.findOne({ studentId: req.user_cxt.studentId }).select({ hierarchy: 1, orientation: 1, _id: 0 });
-    const getTextBookCode = contentMapping.findOne({ "assetId": assetId }).select({ "content.category": 1, coins: 1, "refs.textbook.code": 1, _id: 0 });
-    const promiseData = await Promise.all([getStudentOrientation, getTextBookCode]);
+    const studentData = await Student.findOne({ active: true, studentId: req.user_cxt.studentId }, { hierarchyLevels: 1, orientation: 1, _id: 0 });
+    const orientation = studentData.orientation;
+    const className = studentData.hierarchyLevels.L_2;
+    const branchName = studentData.hierarchyLevels.L_5;
+    const textbookCode = assetData.refs.textbook.code;
+    const categoryName = assetData.content.category;
+    const textbookData = await Textbook.findOne({ active: true, code: textbookCode, orientations: { $in: [null, '', orientation] }, branches: { $in: [null, '', branchName] } }, { _id: 1 });
 
-    const orientationName = promiseData[0].orientation;
-    const className = promiseData[0].hierarchy[1].child;
-    const branchName = promiseData[0].hierarchy[3].child;
-    const textbookCode = promiseData[1].refs.textbook.code;
-    const categoryName = promiseData[1].content.category;
-
-    const getTextBookOrientationandBranch = await textbook.findOne({ code: textbookCode }).select({ orientations: 1, _id: 0, branches: 1 });
-    if (!(getTextBookOrientationandBranch.orientations.indexOf(orientationName) !== -1 || getTextBookOrientationandBranch.branches.indexOf(branchName) !== -1)) {
+    if (!textbookData) {
       return res.status(400).send("This asset is not belong to your course...")
     }
 
-    const studentQuery = studentLedger.findOne({ studentId: req.user_cxt.studentId, "assetId": assetId });
-    const contentMappingQuery = contentMapping.findOne({ "assetId": assetId }).select({ coins: 1, _id: 0 });
-    const [studentLedgerResult, contentMappingResult] = await Promise.all([studentQuery, contentMappingQuery]);
 
-    if (!contentMappingResult) {
-      return res.status(400).send("Bad Asset Id")
-    }
+    const studentLedgerResult = await StudentLedger.findOne({ studentId: req.user_cxt.studentId, assetId });
     if (studentLedgerResult) {
       return res.status(405).send("You have already earned coins for this Asset..");
     }
-    let Data = new studentLedger({
-      coins: parseFloat(contentMappingResult.coins),
+
+    let Data = new StudentLedger({
+      coins: parseFloat(assetData.coins),
       studentId: req.user_cxt.studentId,
       transactionType: "credit",
-      assetId: assetId,
+      assetId,
       "refs.branch.name": branchName,
-      "refs.orientation.name": orientationName,
+      "refs.orientation.name": orientation,
       "refs.class.name": className,
       category: categoryName
     });
     await Data.save();
 
-    return res.status(200).send(`${parseFloat(contentMappingResult.coins)} credited to your account.`);
+    return res.status(200).send(`${parseFloat(assetData.coins)} credited to your account.`);
   } catch (err) {
+    console.log(err)
     return res.status(500).send("Internal server error.");
   };
 }
@@ -82,65 +80,75 @@ export async function debitCoin(req, res) {
       return res.status(400).send("Asset Id is missing.");
     }
     const studentId = req.user_cxt.studentId;
-    const [studentLedger, contentMapping, student, textbook] = await Promise.all([studentLedgerModel(req.user_cxt), contentMappingModel(req.user_cxt), studentModel(req.user_cxt), textbookModel(req.user_cxt)]);
+    const [
+      StudentLedger,
+      ContentMapping,
+      Student,
+      Textbook] = await Promise.all([
+        StudentLedgerModel(req.user_cxt),
+        ContentMappingModel(req.user_cxt),
+        StudentModel(req.user_cxt),
+        TextbookModel(req.user_cxt)
+      ]);
 
     //Check for if AssetId Is present in collection or not!!!
-    const confirmation = await contentMapping.findOne({ "assetId": assetId });
-    if (!confirmation) {
+    const assetData = await ContentMapping.findOne({ active: true, assetId });
+    if (!assetData) {
       return res.status(404).send("this is invalid asset id...");
     }
-
+    //if no coins for asset id
+    // if (!contentMappingData.coins) {
+    //   return res.status(400).send("Bad Asset Id, this id have no coins")
+    // }
     //validate assetId If this id is belong to studentId
-    const getStudentOrientation = student.findOne({ studentId: req.user_cxt.studentId }).select({ hierarchy: 1, orientation: 1, _id: 0 });
-    const getTextBookCode = contentMapping.findOne({ "assetId": assetId }).select({ "content.category": 1, coins: 1, "refs.textbook.code": 1, _id: 0 });
-    const promiseData = await Promise.all([getStudentOrientation, getTextBookCode]);
-
-    const orientationName = promiseData[0].orientation;
-    const className = promiseData[0].hierarchy[1].child;
-    const branchName = promiseData[0].hierarchy[3].child;
-    const textbookCode = promiseData[1].refs.textbook.code;
-    const categoryName = promiseData[1].content.category;
-
-    const getTextBookOrientationandBranch = await textbook.findOne({ code: textbookCode }).select({ orientations: 1, _id: 0, branches: 1 });
-    if (!(getTextBookOrientationandBranch.orientations.indexOf(orientationName) !== -1 || getTextBookOrientationandBranch.branches.indexOf(branchName) !== -1)) {
+    const studentData = await Student.findOne({ active: true, studentId: req.user_cxt.studentId }, { hierarchyLevels: 1, orientation: 1, _id: 0 });
+    const orientation = studentData.orientation;
+    const className = studentData.hierarchyLevels.L_2;
+    const branchName = studentData.hierarchyLevels.L_5;
+    const textbookCode = assetData.refs.textbook.code;
+    const categoryName = assetData.content.category;
+    const textbookData = await Textbook.findOne({ active: true, code: textbookCode, orientations: { $in: [null, '', orientation] }, branches: { $in: [null, '', branchName] } }, { _id: 1 });
+    if (!textbookData) {
       return res.status(400).send("This asset is not belong to your course...")
     }
 
-    const [studentLedgerResult, contentMappingResult, StudentAllCoins] = await Promise.all(
-      [studentLedger.findOne({ studentId: req.user_cxt.studentId, "assetId": assetId }),
-      contentMapping.findOne({ "assetId": assetId }, { coins: 1 }),
-      studentLedger.find({ "studentId": studentId }, { coins: 1 })]);
-
-    if (!contentMappingResult) {
-      return res.status(400).send("Bad Asset Id")
+    const [
+      checkUnlockAssetId,
+      sumOfCoins] = await Promise.all([
+        StudentLedger.findOne({ studentId, assetId }),
+        StudentLedger.aggregate([
+          { $match: { studentId } },
+          { $group: { _id: "$studentId", coins: { $sum: "$coins" } } }
+        ])]);
+    if (!sumOfCoins.length) {
+      return res.status(404).send("You have no created operation till now...");
     }
-    if (!studentLedgerResult) {
+    if (checkUnlockAssetId) {
       //check for available coin or not 
-      let sumOfCoin = 0;
-      for (let i = 0; i < StudentAllCoins.length; i++) {
-        sumOfCoin = sumOfCoin + StudentAllCoins[i].coins;
-      }
-      if (sumOfCoin < Math.abs(contentMappingResult.coins)) {
-        return res.status(299).send("Sorry! But you have not sufficient ammount of coin to unlock this game...");
-
-      } else {
-        let Data = new studentLedger({
-          coins: parseFloat(contentMappingResult.coins),
-          studentId: req.user_cxt.studentId,
-          transactionType: "debit",
-          assetId: assetId,
-          "refs.branch.name": branchName,
-          "refs.orientation.name": orientationName,
-          "refs.class.name": className,
-          category: categoryName
-        });
-        await Data.save();
-        return res.status(200).send(`${parseFloat(contentMappingResult.coins)} debit from your account.`);
-      }
-    } else {
       return res.status(406).send("You have already unlock  this asset...");
     }
+
+    let sumOfCoin = sumOfCoins[0].coins;
+    if ((Math.abs(sumOfCoin) - Math.abs(assetData.coins) < 0)) {
+      return res.status(299).send("Sorry! But you have not sufficient ammount of coin to unlock this game...");
+
+    }
+    let Data = new StudentLedger({
+      coins: parseFloat(assetData.coins),
+      studentId: req.user_cxt.studentId,
+      transactionType: "debit",
+      assetId: assetId,
+      "refs.branch.name": branchName,
+      "refs.orientation.name": orientation,
+      "refs.class.name": className,
+      category: categoryName
+    });
+    await Data.save();
+    return res.status(200).send(`${parseFloat(assetData.coins)} debit from your account.`);
+
+
   } catch (err) {
+    console.log("err : ", err)
     return res.status(500).send("Internal server error.");
   };
 }
@@ -154,24 +162,29 @@ export async function getQuery(req) {
 }
 export async function studentCoinLog(req, res) {
   try {
+    const studentId = req.user_cxt.studentId;
     //statementType three kind 1. All 2. debit 3. Credit
-    const studentLedger = await studentLedgerModel(req.user_cxt);
+    const StudentLedger = await StudentLedgerModel(req.user_cxt);
     const query = await getQuery(req);
-    const studentLedgerPromise = await Promise.all([studentLedger.find(query).sort({ "created_at": -1 })]);
-    const studentLedgerData = studentLedgerPromise[0];
-    let sumOfCoin = 0;
-    for (let i = 0; i < studentLedgerData.length; i++) {
-      sumOfCoin = sumOfCoin + studentLedgerData[i].coins;
+    const [studentLedgerData, sumOfCoins] = await Promise.all([StudentLedger.find(query, { refs: 0 }).sort({ "created_at": -1 }),
+    StudentLedger.aggregate([
+      { $match: { studentId } },
+      { $group: { _id: "$studentId", coins: { $sum: "$coins" } } }
+    ])]);
+
+    if (!sumOfCoins.length) {
+      return res.status(404).send("You have no created operation till now...");
     }
     const finalBalance = {
-      "finalBalance": sumOfCoin
+      "finalBalance": sumOfCoins[0].coins
     }
     studentLedgerData.push(finalBalance);
     return res.json(studentLedgerData);
+
   }
   catch (err) {
+    console.log(err)
     return res.status(500).send("Internal server error.");
   }
 }
 export default { creditCoin, debitCoin, studentCoinLog };
-
