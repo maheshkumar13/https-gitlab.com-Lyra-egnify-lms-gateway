4c9fc80bc495ecb4abba1c3cd13896bf1eea9f0f
NikhilKrDwivedi <authornikhildwivedi@gmail.com>
2019-11-05 11:34:49 +0530

    student coin ledger

--- src/api/studentLedger/studentLedger.controller.js
+++ src/api/studentLedger/studentLedger.controller.js
@@ -13,7 +13,11 @@ export async function creditCoin(req, res) {
     const { assetId } = req.params;
     //check for if AssetId will missing.
     if (!assetId) {
-      return res.status(400).send("Asset Id is missing.");
+      return res.status(400).json({
+        status: "failure",
+        message: "assetId is missing !!!",
+        data: ""
+      });
     }
 
     const [
@@ -30,7 +34,11 @@ export async function creditCoin(req, res) {
     //Check for if AssetId Is present in collection or not!!!
     const assetData = await ContentMapping.findOne({ assetId, active: true });
     if (!assetData) {
-      return res.status(404).send("this is invalid asset id...");
+      return res.status(404).json({
+        status: "failure",
+        message: "this is invalid asset id !!!",
+        data: ""
+      });
     }
 
     //validate assetId If this id is belong to studentId
@@ -43,13 +51,21 @@ export async function creditCoin(req, res) {
     const textbookData = await Textbook.findOne({ active: true, code: textbookCode, orientations: { $in: [null, '', orientation] }, branches: { $in: [null, '', branchName] } }, { _id: 1 });
 
     if (!textbookData) {
-      return res.status(400).send("This asset is not belong to your course...")
+      return res.status(400).json({
+        status: "failure",
+        message: "This asset is not belong to your course!!!",
+        data: ""
+      });
     }
 
 
     const studentLedgerResult = await StudentLedger.findOne({ studentId: req.user_cxt.studentId, assetId });
     if (studentLedgerResult) {
-      return res.status(405).send("You have already earned coins for this Asset..");
+      return res.status(405).json({
+        status: "failure",
+        message: "You have already earned coins for this Asset !!!",
+        data: ""
+      });
     }
 
     let Data = new StudentLedger({
@@ -64,10 +80,18 @@ export async function creditCoin(req, res) {
     });
     await Data.save();
 
-    return res.status(200).send(`${parseFloat(assetData.coins)} credited to your account.`);
+    return res.status(200).json({
+      status: "success",
+      message: `${parseFloat(assetData.coins)} coins has been credited to your account.`,
+      data: `${ parseFloat(assetData.coins)}`,
+    });
   } catch (err) {
     console.log(err)
-    return res.status(500).send("Internal server error.");
+    return res.status(500).json({
+      status: "failure",
+      message: "Internal Server Error !!!",
+      data: ""
+    });
   };
 }
 
@@ -77,7 +101,11 @@ export async function debitCoin(req, res) {
     const { assetId } = req.params;
     //check for if AssetId will missing.
     if (!assetId) {
-      return res.status(400).send("Asset Id is missing.");
+      return res.status(400).json({
+        status: "failure",
+        message: "assetId is missing !!!",
+        data: ""
+      });
     }
     const studentId = req.user_cxt.studentId;
     const [
@@ -94,12 +122,13 @@ export async function debitCoin(req, res) {
     //Check for if AssetId Is present in collection or not!!!
     const assetData = await ContentMapping.findOne({ active: true, assetId });
     if (!assetData) {
-      return res.status(404).send("this is invalid asset id...");
+      return res.status(404).json({
+        status: "failure",
+        message: "this is invalid asset id !!!",
+        data: ""
+      });
     }
-    //if no coins for asset id
-    // if (!contentMappingData.coins) {
-    //   return res.status(400).send("Bad Asset Id, this id have no coins")
-    // }
+    
     //validate assetId If this id is belong to studentId
     const studentData = await Student.findOne({ active: true, studentId: req.user_cxt.studentId }, { hierarchyLevels: 1, orientation: 1, _id: 0 });
     const orientation = studentData.orientation;
@@ -109,7 +138,11 @@ export async function debitCoin(req, res) {
     const categoryName = assetData.content.category;
     const textbookData = await Textbook.findOne({ active: true, code: textbookCode, orientations: { $in: [null, '', orientation] }, branches: { $in: [null, '', branchName] } }, { _id: 1 });
     if (!textbookData) {
-      return res.status(400).send("This asset is not belong to your course...")
+      return res.status(400).json({
+        status: "failure",
+        message: "This asset is not belong to your course!!!",
+        data: ""
+      });
     }
 
     const [
@@ -121,16 +154,28 @@ export async function debitCoin(req, res) {
           { $group: { _id: "$studentId", coins: { $sum: "$coins" } } }
         ])]);
     if (!sumOfCoins.length) {
-      return res.status(404).send("You have no created operation till now...");
+      return res.status(404).json({
+        status: "failure",
+        message: "You have no created operation till now !!!",
+        data: ""
+      });
     }
     if (checkUnlockAssetId) {
       //check for available coin or not 
-      return res.status(406).send("You have already unlock  this asset...");
+      return res.status(406).json({
+        status:"success",
+        message: "You have already unlock this asset !!!",
+        data:""
+      });
     }
 
     let sumOfCoin = sumOfCoins[0].coins;
     if ((Math.abs(sumOfCoin) - Math.abs(assetData.coins) < 0)) {
-      return res.status(299).send("Sorry! But you have not sufficient ammount of coin to unlock this game...");
+      return res.status(299).json({
+        status: "failure",
+        message: "Sorry! But you have not sufficient ammount of coin to unlock this game !!!",
+        data: ""
+      });
 
     }
     let Data = new StudentLedger({
@@ -144,12 +189,20 @@ export async function debitCoin(req, res) {
       category: categoryName
     });
     await Data.save();
-    return res.status(200).send(`${parseFloat(assetData.coins)} debit from your account.`);
+    return res.status(200).json({
+      status: "success",
+      message: `${parseFloat(assetData.coins)} debit from your account.`,
+      data: `${parseFloat(assetData.coins)}`,
+    });
 
 
   } catch (err) {
     console.log("err : ", err)
-    return res.status(500).send("Internal server error.");
+    return res.status(500).json({
+      status: "failure",
+      message: "Internal Server Error !!!",
+      data: ""
+    });
   };
 }
 
@@ -173,18 +226,29 @@ export async function studentCoinLog(req, res) {
     ])]);
 
     if (!sumOfCoins.length) {
-      return res.status(404).send("You have no created operation till now...");
+      return res.status(404).json({
+        status: "failure",
+        message: "You have no created operation till now !!!",
+        data: ""
+      });
     }
-    const finalBalance = {
-      "finalBalance": sumOfCoins[0].coins
-    }
-    studentLedgerData.push(finalBalance);
-    return res.json(studentLedgerData);
+    
+    let resData={
+      transaction: studentLedgerData,
+      finalBalance: sumOfCoins[0].coins
+    };
+
+    
+    return res.json(resData);
 
   }
   catch (err) {
     console.log(err)
-    return res.status(500).send("Internal server error.");
+    return res.status(500).json({
+      status: "failure",
+      message: "Internal Server Error !!!",
+      data: ""
+    });
   }
 }
 export default { creditCoin, debitCoin, studentCoinLog };
