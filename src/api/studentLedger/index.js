8d15a8eb6a6bb58f5d2e092c53bf1acfabbcffe6
NikhilKrDwivedi <authornikhildwivedi@gmail.com>
2019-11-11 16:54:39 +0530

    Final Balance Added

--- src/api/studentLedger/index.js
+++ src/api/studentLedger/index.js
@@ -14,5 +14,6 @@ const controller = require('./studentLedger.controller');
 router.post('/credit/:assetId', controller.creditCoin);
 router.post('/debit/:assetId', controller.debitCoin);
 router.post('/coinLog',  controller.studentCoinLog);
+router.post('/finalBalance',  controller.finalBalance);
 
 module.exports = router;
