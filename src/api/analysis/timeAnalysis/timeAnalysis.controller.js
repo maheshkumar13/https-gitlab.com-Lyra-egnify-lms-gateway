bca1d2e59a4f33da78a8446af1ee5eba461eeb8c
NikhilKrDwivedi <authornikhildwivedi@gmail.com>
2019-12-09 11:49:43 +0530

    Student Day wise sorting

--- src/api/analysis/timeAnalysis/timeAnalysis.controller.js
+++ src/api/analysis/timeAnalysis/timeAnalysis.controller.js
@@ -250,7 +250,6 @@ export async function getTimeAnalysisStudentsList(args, context) {
   const data = objsData && objsData.length ? objsData : [];
   return [count, data];
 }
-/*********************************************************************************/
 
 export async function getTimeAnalysisStudentsListByDay(args, context) {
   const TimeAnalysis = await TimeAnalysisModel(context);
@@ -266,16 +265,14 @@ export async function getTimeAnalysisStudentsListByDay(args, context) {
     query.date.$lte = args.endDate;
   }
   const skip = (args.pageNumber - 1) * args.limit;
-  if (!args.limit) args.limit = 1;
+  if (!args.limit) args.limit = 10;
   let groupQuery = {
     _id: '$_id.studentId', studentName: { $first: '$studentName' },
     totalTimeSpent: { $sum: '$totalTimeSpent' },
     data: { $push: { weekDay: '$_id.weekDay', totalTimeSpent: '$totalTimeSpent' } },
   }
-  let sortQuery
-  if (args.sortBy === 'studentName') {
-    sortQuery = { studentName: args.sortType }
-  }
+  let sortQuery = { studentName: args.sortType }
+  
   if (args.sortBy === 'totalTimeSpent') {
     sortQuery = { totalTimeSpent: args.sortType }
   }
