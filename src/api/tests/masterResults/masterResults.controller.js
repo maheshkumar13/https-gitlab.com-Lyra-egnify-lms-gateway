11df8a4747b5f6e9ebddd99282d8240803969639
eatmore <sarthak.egnify@gmail.com>
2019-12-10 09:44:12 +0530

    paperid and branch wise student practice test list added

--- src/api/tests/masterResults/masterResults.controller.js
+++ src/api/tests/masterResults/masterResults.controller.js
@@ -1,4 +1,5 @@
 import { getModel as MasterResultModel } from '../masterResults/masterResults.model';
+import { getModel as StudentModel } from '../../settings/student/student.model';
 
 export async function getMasterResults(args, context) {
   args = args.input; //eslint-disable-line
@@ -34,4 +35,46 @@ export async function getMasterResults(args, context) {
   }));
 }
 
-export default { getMasterResults };
+export async function getStudentList(req, res){
+  if(!req.params || !req.params.paperId){
+      throw new Error('Please provide questionPaperId');
+  }
+  const branchName = req.user_cxt.rawHierarchy[4].child;
+  if(!branchName){
+      throw new Error('branchName is not available');
+  }
+  const Student = await StudentModel(req.user_cxt);
+  const masterresults = await MasterResultModel(req.user_cxt);
+  const studentInfo = await Student.aggregate([{
+  $match : {"hierarchy.child" : branchName}},
+   {$group : {_id : {studentId : "$studentId", 
+   studentName : "$studentName"}}}]);
+  const studentIdArray = studentInfo.map(x => x._id.studentId);
+  if(!studentIdArray.length){
+      console.error("No practice data is available");
+  }
+  const studentNameArray = studentInfo.map(x => x._id.studentName);
+  if(studentNameArray.length !== studentIdArray.length){
+      throw new Error('Data is missing');
+  }
+  const masterresultData = await masterresults
+   .aggregate([{$match : {questionPaperId : req.params.paperId,
+   studentId : {$in : studentIdArray}}} 
+   ,{$sort : {"updated_at" : -1}},  
+   {"$group": {"_id": "$studentId", 
+   "cwuAnalysis" : {"$first" : "$cwuAnalysis"}, 
+   "obtainedMarks": {"$first" : "$obtainedMarks"}}}]);
+  //res.send(masterresultData);
+  for(let i = 0; i < masterresultData.length; i += 1){
+      let index = 0;
+      if(studentIdArray.includes(masterresultData[i]._id)){
+          index = studentIdArray.indexOf(masterresultData[i]._id);
+          masterresultData[i].studentName = studentNameArray[index];
+      }
+  }
+  res.send(masterresultData);
+}
+
+
+
+export default { getMasterResults, getStudentList };
