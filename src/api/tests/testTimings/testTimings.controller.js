bd5011f6527823061dc4058968538560e94326bd
NikhilKrDwivedi <nikhilkumar.egnify@gmail.com>
2020-01-03 14:50:21 +0530

    testTiming information added

--- src/api/tests/testTimings/testTimings.controller.js
+++ src/api/tests/testTimings/testTimings.controller.js
@@ -18,7 +18,7 @@ export async function getTestTimings(req, res) {
             });
         }
         //validate if giving testId is valid or not
-        let [testIdConfirmation,branchConfirmation]= await Promise.all([TestTimings.findOne({testId}),InstituteHierarchy.findOne({childCode:branch})]);
+        let [testIdConfirmation,branchConfirmation]= await Promise.all([TestTimings.findOne({testId}),InstituteHierarchy.findOne({child:branch})]);
         if(!testIdConfirmation){
             return res.status(400).json({
                 status: "failure",
@@ -37,7 +37,7 @@ export async function getTestTimings(req, res) {
         //prepare query for match 
         const matchQuery = {testId}
         if(req.user_cxt.hierarchy.length>=5 && !branch) matchQuery.hierarchyId={$in:req.user_cxt.hierarchy.map(x=>x.childCode)}
-        if(branch) matchQuery.hierarchyId=branch
+        if(branch) matchQuery.hierarchyId=branchConfirmation.childCode
         //query test data !!!
         const count = await TestTimings.count(matchQuery);
         if(limit<=0){
@@ -45,14 +45,31 @@ export async function getTestTimings(req, res) {
             skip=0
         } 
         const resData = await TestTimings.aggregate([{$match:matchQuery},
-            {$lookup: {from:'institutehierarchies', localField: 'hierarchyId', 
-            foreignField:'childCode', as: "branch"}},{$skip:skip},{$limit:limit},
-            {$project:{_id:1,testId:1,class:1,orientations:1,startTime:1,endTime:1,
-            duration:1,'branch.childCode':1,'branch.child':1}}]);
+            {$lookup: {
+                from:'institutehierarchies', 
+                localField: 'hierarchyId', 
+                foreignField:'childCode', 
+                as: "branch"}
+            },
+            { $unwind:"$branch" },
+            {$lookup: { 
+                from: "tests",
+                localField: "testId", 
+                foreignField: "testId",
+                as: "testData"}
+            },
+            { $unwind:"$testData" },
+            {$skip:skip},
+            {$limit:limit},
+            {$project:{
+                _id:1,testId:1,class:1,orientations:1,startTime:1,endTime:1,
+                duration:1,'branch.childCode':1,'branch.child':1,'testData.testName':1}
+            }
+        ]);
         if (!resData) {
             return res.status(204).json({
                 status: "failure",
-                message: "this is invalid question paper id !!!",
+                message: "No Data Found !!!",
                 data:[]
             });
         }
