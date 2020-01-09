19b2566307dca5f8b9eb2170d029704554481025
Mohit <mohit@egnify.com>
2020-01-09 13:52:05 +0530

    publish quiz api added

--- src/api/settings/contentMapping/contentMapping.controller.js
+++ src/api/settings/contentMapping/contentMapping.controller.js
@@ -5,6 +5,7 @@ import { getModel as ConceptTaxonomyModel } from '../conceptTaxonomy/concpetTaxo
 import { getModel as ContentMappingModel } from './contentMapping.model';
 import { getModel as InstituteHierarchyModel } from '../instituteHierarchy/instituteHierarchy.model';
 import { getModel as studentInfoModel } from '../student/student.model';
+import { getModel as Questions } from '../../tests/questions/questions.model';
 
 import { config } from '../../../config/environment';
 import { getStudentData } from '../textbook/textbook.controller';
@@ -2269,6 +2270,33 @@ export async function getContentMappingUploadedDataReadingMaterialAudio(args,con
       data
     }
   })
+}
+
+export async function publishQuiz(req, res){
+  try{
+    const assetId = req.params.assetId;
+    const questionPaperId = req.body.questionPaperId;
+    if(!questionPaperId){
+      return res.status(400).send("Question paper id missing.");
+    }
+    const [ContentSchema,QuestionSchema] = await Promise.all([
+      ContentMappingModel(req.user_cxt),Questions(req.user_cxt)
+    ]);
+    const questionsCount = await QuestionSchema.count({questionPaperId});
+    if(!questionsCount){
+      return res.status(400).send("Invalid question paper id");
+    }
+    const setObj = {
+      "metaData.questionPaperId": questionPaperId
+    };
+    const content = await ContentSchema.findOneAndUpdate({assetId},{$set: setObj},{new: true}).select({_id: 1}).lean();
+    if(!content){
+      return res.status(400).send("Invalid asset id.");
+    }
+    return res.status(200).send("Success");
+  }catch(err){
+    return res.status(500).send("internal server error.");
+  }
 }
 
 export default{
