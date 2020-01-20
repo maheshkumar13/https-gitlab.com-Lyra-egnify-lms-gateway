f7dfb08a9a2aaca358ff3c74eaf50940a0e73092
Mohit <mohit@egnify.com>
2020-01-20 15:10:24 +0530

    change in the port of parser

--- src/config/environment/uat.js
+++ src/config/environment/uat.js
@@ -40,6 +40,6 @@ module.exports = {
   AWS_S3_REGION: process.env.AWS_S3_REGION || 'ap-south-1',
   AWS_PUBLIC_BUCKET_FOLDER: 'samplefiles',
   parser:{
-    uri : process.env.PARSER_SERVICE || "http://uat.elk.rankguru.com:7000/parse/"
+    uri : process.env.PARSER_SERVICE || "http://uat.elk.rankguru.com:3000/parse/"
   },
 };
