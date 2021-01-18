043f349d50a4a2128bb1e35d5e0f723a435447bc
Sumalatha Bareddy <suma@egnify.com>
2021-01-18 19:33:41 +0530

    live class -apis

--- src/api/live/index.js
+++ src/api/live/index.js
@@ -1,13 +1,4 @@
 const express = require('express');
-const Multer = require('multer');
-
-const multer = Multer({
-  storage: Multer.MemoryStorage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb
-  },
-})
-
 const router = express.Router();
 
 const controller = require('./class.controller');
